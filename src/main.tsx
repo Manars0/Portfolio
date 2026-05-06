80e7f1080655f7beb5d159b4b9e2255cb3c72291
Manar Salem <206032564+Manars0@users.noreply.github.com>
2026-05-07 01:59:58 +0300

    fix vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import * as React from "react";
   import { createRoot } from "react-dom/client";
-  import App from "./app/./App.jsx";
+  import App from "./app/./App.tsx";
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
