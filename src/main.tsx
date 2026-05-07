955df297265b9bd4a4e7e22e4f464bbd4d51756d
Manar Salem <206032564+Manars0@users.noreply.github.com>
2026-05-07 03:27:13 +0300

    fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import "./styles/index.css";
-import App from "./app/App.tsx";
+import App from "./app/App";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(<App />);
